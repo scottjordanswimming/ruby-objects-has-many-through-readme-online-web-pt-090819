0f7cf9fdc722026e7ed2cb34f6a4607b2c75e66a
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-20 19:43:55 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
 class Meal
+  
+  attr_accessor :name, :years
+  
+  @@all = []
+  
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+    
+  end
+  
+  def self.all 
+    
+    
+    @@all 
+    
+  end
 
 end
