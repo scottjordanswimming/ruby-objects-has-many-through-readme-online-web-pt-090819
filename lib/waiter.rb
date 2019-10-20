b16dcf720ebb6528b606a84148f16f7c90e8b042
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-20 19:56:16 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -26,7 +26,7 @@ end
 def best_tipper 
   best_tipped_meal = meals.max do 
     |meal_a, meal_b|
-    meal.a.tip <=> meal_b.tip 
+    meal_a.tip <=> meal_b.tip 
   end 
   best_tipped_meal.cusomter 
 end
